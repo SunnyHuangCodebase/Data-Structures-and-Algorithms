a2defb97b4c0086279dba736961c6b1d62655646
SunnyHuangCodebase <106368104+SunnyHuangCodebase@users.noreply.github.com>
2022-10-22 19:57:57 -0400

    Add Weighted Graph Module
    Create weighted_graph.py.
    
    Add algorithm to get shortest distance between two nodes.
    
    Add tests.

---
+++ data_structures/weighted_graph.py
@@ -0,0 +1,116 @@
+from __future__ import annotations
+import heapq
+
+
+class WeightedGraph:
+
+  class Node:
+    value: str
+    edges: dict[str, WeightedGraph.Edge]
+
+    def __init__(self, value: str) -> None:
+      self.value = value
+      self.edges = {}
+
+    def __str__(self):
+      return self.value
+
+    def __repr__(self):
+      return self.value
+
+    def __lt__(self, other: WeightedGraph.Node):
+      return ord(self.value) < ord(other.value)
+
+    def __le__(self, other: WeightedGraph.Node):
+      return ord(self.value) <= ord(other.value)
+
+    def __gt__(self, other: WeightedGraph.Node):
+      return ord(self.value) > ord(other.value)
+
+    def __ge__(self, other: WeightedGraph.Node):
+      return ord(self.value) >= ord(other.value)
+
+    def add_edge(self, node: WeightedGraph.Node, weight: int):
+      """Adds a weighted edge to another node."""
+      self.edges[node.value] = WeightedGraph.Edge(self, node, weight)
+
+    def get_edges(self):
+      return self.edges.values()
+
+  class Edge:
+    source: WeightedGraph.Node
+    target: WeightedGraph.Node
+    weight: int
+
+    def __init__(self, source: WeightedGraph.Node, target: WeightedGraph.Node,
+                 weight: int) -> None:
+      self.source = source
+      self.target = target
+      self.weight = weight
+
+    def __str__(self):
+      return f"{self.source} -{self.weight})-> {self.target}"
+
+  nodes: dict[str, WeightedGraph.Node]
+
+  def __init__(self):
+    self.nodes = {}
+
+  def add_node(self, name: str):
+    """Adds a node if it doesn't exist."""
+    self.nodes[name] = self.nodes.get(name, self.Node(name))
+
+  def add_edge(self, source: str, target: str, weight: int):
+    """Adds a weighted edge between two nodes."""
+    source_node = self.nodes[source]
+    target_node = self.nodes[target]
+    source_node.add_edge(target_node, weight)
+    target_node.add_edge(source_node, weight)
+
+  def __str__(self):
+    output: list[str] = []
+    for node in self.nodes.values():
+      output.append(f"{node} is connected to {node.get_edges()}")
+    return "\n".join(output)
+
+  def get_shortest_distance(self, source: str, target: str) -> int:
+    """Returns the shortest distance between two nodes.
+
+    Uses Djikstra's Algorithm.
+    """
+    try:
+      if not self.nodes[target].get_edges():
+        raise PathNotFoundError
+    except KeyError:
+      raise NonexistentNode
+
+    heap: list[tuple[int, WeightedGraph.Node]] = []
+    visited: set[WeightedGraph.Node] = set()
+    heapq.heappush(heap, (0, self.nodes[source]))
+
+    while heap:
+
+      weight, node = heapq.heappop(heap)
+
+      if node.value is target:
+        return weight
+
+      if node in visited:
+        continue
+
+      visited.add(node)
+
+      for edge in node.get_edges():
+        heapq.heappush(heap, (weight + edge.weight, edge.target))
+
+    raise PathNotFoundError
+
+
+class PathNotFoundError(Exception):
+  """Path between two graph nodes not found."""
+
+
+class NonexistentNode(Exception):
+  """Node not found in graph."""
+
+
---
+++ tests/test_weighted_graph.py
@@ -0,0 +1,63 @@
+import pytest
+import textwrap
+from pytest import CaptureFixture, MonkeyPatch, TempPathFactory
+
+from data_structures.weighted_graph import NonexistentNode, PathNotFoundError, WeightedGraph
+
+
+class TestWeightedGraph:
+
+  @pytest.fixture
+  def node_labels(self) -> list[str]:
+    return [chr(i) for i in range(ord("A"), ord("G"))]
+
+  @pytest.fixture
+  def weighted_edges(self) -> list[tuple[str, str, int]]:
+
+    return [
+        ("A", "B", 1),
+        ("A", "C", 2),
+        ("A", "D", 5),
+        ("B", "D", 3),
+        ("B", "E", 7),
+        ("C", "D", 1),
+        ("D", "E", 2),
+    ]
+
+  @pytest.fixture
+  def weighted_graph(
+      self, node_labels: list[str],
+      weighted_edges: list[tuple[str, str, int]]) -> WeightedGraph:
+    weighted_graph = WeightedGraph()
+
+    for node in node_labels:
+      weighted_graph.add_node(node)
+
+    for source, target, weight in weighted_edges:
+      weighted_graph.add_edge(source, target, weight)
+    return weighted_graph
+
+  def test_weighted_graph(self, weighted_graph: WeightedGraph):
+    print(weighted_graph)
+
+  def test_weighted_graph_get_shortest_distance(self,
+                                                weighted_graph: WeightedGraph):
+
+    assert weighted_graph.get_shortest_distance("A", "A") == 0
+    assert weighted_graph.get_shortest_distance("A", "B") == 1
+    assert weighted_graph.get_shortest_distance("B", "A") == 1
+    assert weighted_graph.get_shortest_distance("A", "C") == 2
+    assert weighted_graph.get_shortest_distance("A", "D") == 3
+    assert weighted_graph.get_shortest_distance("A", "E") == 5
+    assert weighted_graph.get_shortest_distance("B", "D") == 3
+    assert weighted_graph.get_shortest_distance("C", "E") == 3
+
+    with pytest.raises(NonexistentNode):
+      weighted_graph.get_shortest_distance("A", "G")
+
+    with pytest.raises(PathNotFoundError):
+      weighted_graph.get_shortest_distance("A", "F")
+
+
+if __name__ == "__main__":
+  pytest.main([__file__])
